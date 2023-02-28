2a65efac6dc80392c3fe967c1f7134e10bfa2cf0
SebastianIL <a01661179@tec.mx>
2023-02-28 12:11:37 -0600

    Formato de presentacion

--- calculadora/views.py
+++ calculadora/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    #return HttpResponse('<h1> Bienvenidos a la sesión del jueves! </h1>')
+    return render(request,'index.html')

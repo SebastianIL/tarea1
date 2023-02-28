fc74a0157c6378cb57e3c4ea9c2361094d6d8fec
SebastianIL <a01661179@tec.mx>
2023-02-28 12:33:46 -0600

    Se añaden operaciones a la calculadora intento 1

--- calculadora/urls.py
+++ calculadora/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .import views
-
-urlpatterns=[
+from . import views
+urlpatterns= [
     path('',views.index,name='index'),
+    path('proceso',views.proceso,name='proceso'),
+    path('suma',views.suma,name='suma'),
+    path('resta',views.suma,name='resta'),
+    path('bienvenida',views.bienvenida,name='bienvenida'),
+    path('multiplicacion',views.multiplicacion,name='multipliacion'),
+    path('division',views.division,name='division'),
+
 ]
--- calculadora/views.py
+++ calculadora/views.py
@@ -1,8 +1,108 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from json import loads,dumps
+from math import gcd
 
 # Create your views here.
+class Fraccion:
+    def __init__(self, num, den):
+        self.num = num
+        self.den = den
+    def toJSON(self):
+        return dumps(self, default=lambda o:o.__dict__, sort_keys=False, indent=4)
+
+def reduce(num,den):
+    x=gcd(num,den)
+    num= num // x
+    den= den // x
+    return Fraccion(str(num),str(den))
 
 def index(request):
     #return HttpResponse('<h1> Bienvenidos a la sesión del jueves! </h1>')
     return render(request,'index.html')
+
+def proceso(request):
+    nombre = request.POST['nombre']
+    nombre = nombre.upper()
+    return HttpResponse('Hola '+ nombre)
+
+def bienvenida(request):
+    letrero="Bienvenida"
+    return HttpResponse(letrero)
+
+def multiplicacion(request):
+    body_unicode =request.body.decode('utf-8')
+    body = loads(body_unicode)
+    n1=body['n1']
+    d1=body['d1']
+    n2=body['n2']
+    d2=body['d2']
+    
+    p=int(n1)*int(n2)
+    q=int(d1)*int(d2)
+    resultado= reduce(p,q)
+    json_resultado = resultado.toJSON()
+    return HttpResponse(json_resultado,content_type ="text/json-comment-filtered")
+
+@csrf_exempt
+def suma(request):
+    body_unicode =request.body.decode('utf-8')
+    body = loads(body_unicode)
+    n1=body['n1']
+    d1=body['d1']
+    n2=body['n2']
+    d2=body['d2']
+    
+    n11=int(n1)
+    n22=int(n2)
+    d11=int(d1)
+    d22=int(d2)
+    if(d11==d22):
+        p=n11+n22
+        q=d11
+    else:
+        p=(n11*d22)+(n22*d11)
+        q=d11*d22
+    resultado= Fraccion(str(p),str(q))
+    json_resultado = resultado.toJSON()
+    return HttpResponse(json_resultado,content_type ="text/json-comment-filtered")
+
+@csrf_exempt
+def resta(request):
+    body_unicode =request.body.decode('utf-8')
+    body = loads(body_unicode)
+    n1=body['n1']
+    d1=body['d1']
+    n2=body['n2']
+    d2=body['d2']
+
+    n11=int(n1)
+    n22=int(n2)
+    d11=int(d1)
+    d22=int(d2)
+    if(d11==d22):
+        p=n11-n22
+        q=d11
+    else:
+        p=(n11*d22)-(n22*d11)
+        q=d11*d22
+    resultado= reduce(p,q)
+    json_resultado = resultado.toJSON()
+    return HttpResponse(json_resultado,content_type ="text/json-comment-filtered")
+
+@csrf_exempt
+def division(request):
+    body_unicode =request.body.decode('utf-8')
+    body = loads(body_unicode)
+    n1=body['n1']
+    d1=body['d1']
+    n2=body['n2']
+    d2=body['d2']
+
+    p=int(n1)*int(d2)
+    q=int(d1)*int(n2)
+    resultado= reduce(p,q)
+    json_resultado = resultado.toJSON()
+    return HttpResponse(json_resultado,content_type ="text/json-comment-filtered")
+    #return HttpResponse("Division "+p+" / "+q+" = "+n)
